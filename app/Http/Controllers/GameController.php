94eebc9c850d278ff13b40c2315b949cbcc40e8e
AlexCheg89 <aleksejchegodaev@yandex.ru>
2024-09-04 08:14:16 +0500

    edit GameController

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -2,10 +2,48 @@
 
 namespace App\Http\Controllers;
 
-
+use App\Services\GameServiceInterface;
 use Illuminate\Http\Request;
 
 class GameController extends Controller
 {
-    //
+    protected $service;
+
+    public function __construct(GameServiceInterface $service)
+    {
+        $this->service = $service;
+    }
+
+    public function index()
+    {
+        return response()->json($this->service->getAllGames());
+    }
+
+    public function show($id)
+    {
+        $game = $this->service->getGameById($id);
+        return $game ? response()->json($game) : response() -> json(['message' => 'Not Found'], 404);
+    }
+
+    public function store(Request $request)
+    {
+        $game = $this->service->createGame($request->all());
+        return response()->json($game, 201);
+    }
+
+    public function update(Request $request, $id) {
+        $game = $this->service->updateGame($id, $request->all());
+        return $game ? response()->json($game) : responce()->json(['message' => 'Not Found'], 404);
+    }
+
+    public function destroy($id)
+    {
+        $this->service->deleteGame($id);
+        return response()->json(null, 204);
+    }
+
+    public function getByGenre($genre)
+    {
+        return response()->json($this->service->getGamesByGenre($genre));
+    }
 }
